8a189906fd177104ada9dc61849896d7e00f0e5b
Jwoliv <bohdanlishchuk0@gmail.com>
2024-07-24 21:16:00 +0300

    refactor

--- services/mt-core/src/main/java/com/mt/repository/view/DailyAmountReportView.java
+++ services/mt-core/src/main/java/com/mt/repository/view/DailyAmountReportView.java
@@ -1,11 +1,9 @@
 package com.mt.repository.view;
 
 import com.mt.model.User;
-import jakarta.persistence.*;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
-import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.time.LocalDateTime;
